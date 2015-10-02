43edcd903f32a6da41fafa5ac5793abe7520034f
Nick Ray <nick@renttrack.com>
2015-10-01 21:09:48 -0700

    wall stuff

---
+++ Wall.cpp
@@ -0,0 +1,9 @@
+//
+//  Wall.cpp
+//  
+//
+//  Created by Nick Ray on 10/1/15.
+//
+//
+
+#include "Wall.h"
---
+++ Wall.h
@@ -0,0 +1,14 @@
+//
+//  Wall.h
+//  
+//
+//  Created by Nick Ray on 10/1/15.
+//
+//
+
+#ifndef ____Wall__
+#define ____Wall__
+
+#include <stdio.h>
+
+#endif /* defined(____Wall__) */
