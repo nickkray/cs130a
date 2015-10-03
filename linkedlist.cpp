c024c24b1e9e8caa8ffbf6e9b857fcc9210d9207
jamilka <jammze@gmail.com>
2015-10-03 15:11:55 -0700

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -21,78 +21,77 @@ void linkedlist<item>::add(item li){        //add item
         last->prev = currentNode;
         count = 1;
     }
+}
     
     
-    
-    template <typename item>
-    void linkedlist<item>::removeAt(int index){     //remove at index
-        if (index <= count){
-            node* currentNode = last;
-            for (int i = 0; i <= index; i++){
-                currentNode = currentNode->next;
-            }
-            currentNode->prev->next = currentNode->next;
-            currentNode->next->prev = currentNode->prev;
-            delete currentNode;
-            count--;
-        }
-    }
-    
-    
-    template <typename item>
-    item linkedlist<item>::findAt(int index){       //find at index
-        node* currentNode = first;
-        if (index < 0 ) index =count + index;
-        if (index <= count && index > -1){
-            int i = 0;
-            while(i < index){
-                currentNode = currentNode->next;
-                i++;
-            }
-        }
-        
-        return currentNode->data;
-    }
-    
-    
-    linkedlist<item>::First()
-    template <typename item>
-    item linkedlist<item>::First(){     //first item
-        return first->data;
-    }
-    
-    template <typename item>
-    item linkedlist<item>::Last(){      //last item
-        return last->data;
-    }
-    
-    
-    template <typename item>
-    int linkedlist<item>::count(){      //count that shit
-        return count;
-    }
-    
-    
-    template <typename item>
-    void linkedlist<item>::clear(){     //clear that shit
-        node* currentNode = first;
-        while(count > 0){
-            node* tmp = currentNode;
+template <typename item>
+void linkedlist<item>::removeAt(int index){     //remove at index
+    if (index <= count){
+        node* currentNode = last;
+        for (int i = 0; i <= index; i++){
             currentNode = currentNode->next;
-            delete tmp;
-            count--;
+        }
+        currentNode->prev->next = currentNode->next;
+        currentNode->next->prev = currentNode->prev;
+        delete currentNode;
+        count--;
+    }
+}
+    
+    
+template <typename item>
+item linkedlist<item>::findAt const(int index){       //find at index
+    node* currentNode = first;
+    if (index < 0 ) index =count + index;
+    if (index <= count && index > -1){
+        int i = 0;
+        while(i < index){
+            currentNode = currentNode->next;
+            i++;
         }
     }
     
+    return currentNode->data;
+}
     
-    template <typename item>
-    linkedlist<item>::~linkedlist(){    //deconstructor
-        if (count > 0){
-            clear();
-        }
-    }
     
-    template <typename item>
-    linkedlist<item>::linkedlist(){     //constructor
-        count=0;
+template <typename item>
+item linkedlist<item>::First const(){     //first item
+    return first->data;
+}
+
+template <typename item>
+item linkedlist<item>::Last const(){      //last item
+    return last->data;
+}
+
+
+template <typename item>
+int linkedlist<item>::count const(){      //count that shit
+    return count;
+}
+
+
+template <typename item>
+void linkedlist<item>::clear(){     //clear that shit
+    node* currentNode = first;
+    while(count > 0){
+        node* tmp = currentNode;
+        currentNode = currentNode->next;
+        delete tmp;
+        count--;
     }
+}
+
+
+template <typename item>
+linkedlist<item>::~linkedlist(){    //deconstructor
+    if (count > 0){
+        clear();
+    }
+}
+
+template <typename item>
+linkedlist<item>::linkedlist(){     //constructor
+    count=0;
+}
