29164c35ce1c6b7d7aef93d08f66fd7c804d1d06
jamilka <jammze@gmail.com>
2015-10-03 16:39:43 -0700

    Create UserNetwork.h

---
+++ UserNetwork.h
@@ -0,0 +1,19 @@
+#ifndef ____UserNetwork__
+#define ____UserNetwork__
+
+class UserNetwork{
+private:
+    linkedlist<User> userList;
+public:
+    UserNetwork();
+    ~UserNetwork();
+    
+    void addUser(User newUser);
+    void removeUser(string userName); //remove by username
+
+    string printUsers const();
+	linkedlist<User> readUsers const();
+   };
+
+
+#endif
