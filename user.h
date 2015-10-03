e413bf1df070472e9b826f5fb366776ef368716e
jamilka <jammze@gmail.com>
2015-10-03 15:36:36 -0700

    Create user.h

---
+++ user.h
@@ -0,0 +1,36 @@
+#ifndef ____User__
+#define ____User__
+
+
+class User{
+private:
+    Wall userWall;
+    string username;
+	string password;
+	string name;
+	string gender;
+	
+public:
+	User(string newName, string newPass, string newName, string newGen);
+	User(string newName, string newPass, string newName); //chillin on gender
+    ~User();
+    
+	string getUsername cosnt();
+	string getPassword const();
+	string getName const();
+	string getGender const();
+	
+	void setPassword(string newPass);
+	void setName(string newName);
+	void setgender(string newGender);
+	
+	void addWallPost(string text);
+	void addWallPost(string text, int mood);
+	void deleteWallPost();
+	
+	string printUserWall const();
+	void readUserWall(string userWall);
+	
+};
+
+#endif
