a618a049a1bb57221450279136ac93ad74bace22
jamilka <jammze@gmail.com>
2015-10-03 15:38:52 -0700

    Update User.h

--- User.h
+++ User.h
@@ -6,31 +6,29 @@ class User{
 private:
     Wall userWall;
     string username;
-	string password;
-	string name;
-	string gender;
+    string password;
+    string name;
+    string gender;
 	
 public:
-	User(string newName, string newPass, string newName, string newGen);
-	User(string newName, string newPass, string newName); //chillin on gender
+     User(string newName, string newPass, string newName, string newGen);
+     User(string newName, string newPass, string newName); //chillin on gender
     ~User();
     
-	string getUsername cosnt();
-	string getPassword const();
-	string getName const();
-	string getGender const();
+     string getUsername cosnt();
+     string getPassword const();
+     string getName const();
+     string getGender const();
 	
-	void setPassword(string newPass);
-	void setName(string newName);
-	void setgender(string newGender);
+     void setName(string newName);
+     void setgender(string newGender);
 	
-	void addWallPost(string text);
-	void addWallPost(string text, int mood);
-	void deleteWallPost();
-	
-	string printUserWall const();
-	void readUserWall(string userWall);
-	
+     void addWallPost(string text);
+     void addWallPost(string text, int mood);
+     void deleteWallPost();
+     
+     string printUserWall const();
+     void readUserWall(string userWall);
 };
 
 #endif
